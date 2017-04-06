c233e0876b89742fc7e8e80c95bc6cffe3c39394
Slava Kovalenko <vkovalenko@Slavas-MacBook-Pro.local>
2017-04-07 00:14:50 +0300

    Init commit

---
+++ app.js
@@ -0,0 +1,28 @@
+//import modules
+var express = require('express');
+
+//create express app
+var app = express();
+var port = 5000;
+
+//Define static content
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello world!');
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log('Application started on port: ' + port);
+});
