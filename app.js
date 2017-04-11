c15a744b675f555f2847ebc42c1770ff9079b913
Slava Kovalenko <vkovalenko@Slavas-MacBook-Pro.local>
2017-04-11 12:04:36 +0300

    Add books route

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
 var express = require('express');
 
 var app = express();
-
-
 var port = process.env.port ;
+var books = [
+    {
+        'title:': 'Book1',
+        'genre': 'Ganre1',
+        'author': 'Author1',
+        'read': true
+    },
+    {
+        'title:': 'Book2',
+        'genre': 'Ganre2',
+        'author': 'Author2',
+        'read': false
+    },
+];
+
 
 
 app.use(express.static('public'));
+var bookRouter = express.Router();
+
+bookRouter.route('/')
+    .get(function(req, res){
+        res.render('books', {
+        title: 'Books',
+        nav: [
+            {
+                Link: '/books',
+                Text: 'Books'
+            }, 
+            {
+                Link: '/authors',
+                Text: 'authors'
+            }
+            ]});
+    });
+
+bookRouter.route('/single')
+    .get(function(req, res){
+        res.send("hello single book");
+    });
+
+
+app.use('/books',bookRouter);
+
+
+
 
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(req, res) {
     res.render('index', {
+        title: 'My web library',
         nav: [
             {
                 Link: '/books',
